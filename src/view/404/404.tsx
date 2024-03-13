80dd5e5bd648d8c83c16cb7798eb7c786540f3ea
mrHoft <ru.hoft@gmail.com>
2024-03-13 23:34:27 +0300

    feat: 404 page

---
+++ src/view/404/404.tsx
@@ -0,0 +1,13 @@
+import router from '~/utils/Router'
+import styles from './.module.css'
+
+export const NotFoundPage = () => {
+  return (
+    <div className={styles.page__404}>
+      <h1>(404) Page not found</h1>
+      <div className={styles.page__404_link} onClick={() => router.go('/')}>
+        {'\u2190'} Go back
+      </div>
+    </div>
+  )
+}
